bbd003571b43e4f846f899ccf1d5fb8e3e32e024
kursor <skurochkin298@gmail.com>
2023-02-10 21:49:37 +0300

    CoroutinesUtils.kt excluded mavenLocal from repositories using KmmDataLoadingAutomation from mavenCentral added LoaderClient in DI

--- shared/src/commonMain/kotlin/com/cashew/core/DI.kt
+++ shared/src/commonMain/kotlin/com/cashew/core/DI.kt
@@ -1,8 +1,12 @@
 package com.cashew.core
 
 import com.cashew.core.network.HttpClientProvider
+import com.cashew.core.utils.DefaultNetworkCoroutineContext
+import com.kursor.kmmdataloadingautomation.LoaderClient
+import kotlinx.coroutines.CoroutineScope
 import org.koin.dsl.module
 
 val coreModule = module {
     single { HttpClientProvider(Const.BACKEND_URL, get()) }
+    single { LoaderClient(CoroutineScope(DefaultNetworkCoroutineContext)) }
 }
---
+++ shared/src/commonMain/kotlin/com/cashew/core/utils/CoroutineUtils.kt
@@ -0,0 +1,5 @@
+package com.cashew.core.utils
+
+import kotlin.coroutines.CoroutineContext
+
+expect val DefaultNetworkCoroutineContext: CoroutineContext
