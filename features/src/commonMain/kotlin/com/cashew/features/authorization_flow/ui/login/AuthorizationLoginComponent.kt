c1df63e90828d52994eaadbc3bee820a4eba63f4
kursor <skurochkin298@gmail.com>
2023-02-23 23:40:47 +0300

    changed StringResource to StringDesc in Auth Errors and added StringDesc.resolve ext function

--- features/src/commonMain/kotlin/com/cashew/features/authorization_flow/ui/login/AuthorizationLoginComponent.kt
+++ features/src/commonMain/kotlin/com/cashew/features/authorization_flow/ui/login/AuthorizationLoginComponent.kt
@@ -2,6 +2,8 @@ package com.cashew.features.authorization_flow.ui.login
 
 import com.cashew.features.MR
 import dev.icerock.moko.resources.StringResource
+import dev.icerock.moko.resources.desc.StringDesc
+import dev.icerock.moko.resources.desc.desc
 import kotlinx.coroutines.flow.StateFlow
 
 interface AuthorizationLoginComponent {
@@ -20,8 +22,8 @@ interface AuthorizationLoginComponent {
     fun onUsernameTextChanged(changedUsername: String)
     fun onPasswordTextChanged(changedPassword: String)
 
-    sealed class Error(val text: StringResource?) {
-        object InvalidCredentials : Error(MR.strings.login_error_invalid_credentials)
+    sealed class Error(val text: StringDesc?) {
+        object InvalidCredentials : Error(MR.strings.login_error_invalid_credentials.desc())
     }
 
     sealed interface Output {
